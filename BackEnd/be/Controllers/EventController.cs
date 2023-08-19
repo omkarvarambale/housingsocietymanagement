a3bd1763f35466d817e5536fa0b5100e38ee9c25
“omkaradagalegit config --global user.email “omkaradagale4883@gmail.com <omkaradagale4883@gmail.com>
2023-08-19 07:16:47 +0530

    Event Controller added

---
+++ BackEnd/be/Controllers/EventController.cs
@@ -0,0 +1,63 @@
+﻿using be.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class EventController : ApiController
+    {
+        ProjectEntities1 projectEntities = new ProjectEntities1();
+
+        // GET: api/Event
+        public IEnumerable<@event> Get()
+        {
+            return projectEntities.events.ToList();
+        }
+
+        // GET: api/Event/5
+        public @event Get(int id)
+        {
+            return projectEntities.events.Find(id);
+        }
+
+        // POST: api/Event
+        public string Post(@event u)
+        {
+            projectEntities.events.Add(u);
+            int i = projectEntities.SaveChanges();
+            if (i == 1) return "Event Added Successfully.";
+            else return "Failed to add Event.";
+        }
+
+        // PUT: api/Event/5
+        public string Put(int id, @event u)
+        {
+            @event dbevent = projectEntities.events.Find(id);
+            dbevent.eventname = u.eventname;
+            dbevent.description = u.description;
+            dbevent.place = u.place;
+            dbevent.eventdate = u.eventdate;
+            dbevent.eventtime = u.eventtime;
+            dbevent.eventimage = u.eventimage;
+           
+            int i = projectEntities.SaveChanges();
+
+            if (i == 1) return "Event Updated Successfully.";
+            else return "Failed to updated Event.";
+
+        }
+
+        // DELETE: api/Event/5
+        public string Delete(int id)
+        {
+            projectEntities.events.Remove(projectEntities.events.Find(id));
+            int i = projectEntities.SaveChanges();
+            if (i == 1) return "Data deleted Successfully.";
+            else return "Failed to delete data.";
+        }
+    }
+}
