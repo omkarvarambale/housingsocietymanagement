7bf507299475ef27fd5b7d6c2b8a80199b925cde
“omkaradagalegit config --global user.email “omkaradagale4883@gmail.com <omkaradagale4883@gmail.com>
2023-08-19 10:18:20 +0530

    React front end project created

---
+++ FrontEnd/client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is web app from Omkar</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
